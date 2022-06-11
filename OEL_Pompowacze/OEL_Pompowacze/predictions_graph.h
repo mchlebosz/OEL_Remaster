b542aca334d4319be3aef686e7b9a47c302525ce
kubsnn <51138889+kubsnn@users.noreply.github.com>
2022-06-11 02:39:25 +0200

    + cool design, ~ prices generator corrected

--- OEL_Pompowacze/OEL_Pompowacze/predictions_graph.h
+++ OEL_Pompowacze/OEL_Pompowacze/predictions_graph.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <SDL.h>
+#include "label.h"
 #include "game.h"
 #include "vector.h"
 #include "extended_vector.h"
@@ -38,8 +40,8 @@ void prices_draw(SDL_Renderer* renderer, vector* prices) {
 	for (int i = 0; i < prices->size; ++i) {
 		int w = 25;
 		int x = SCREEN_WIDTH / 2 - prices->size * w / 2 + i * w;
-		int y = SCREEN_HEIGHT / 2 + 180;
-		int h = fvector_get(prices, i) * -90;
+		int y = SCREEN_HEIGHT / 2 + 220;
+		int h = fvector_get(prices, i) * -100;
 		SDL_Rect rect = {x, y, w, h};
 		SDL_SetRenderDrawColor(renderer, 143, 47, 101, 255);
 		SDL_RenderFillRect(renderer, &rect);
@@ -47,6 +49,16 @@ void prices_draw(SDL_Renderer* renderer, vector* prices) {
 }
 
 void prices_loop(game_t* game, vector* prices) {
+	const string title = string_create_from_cstring("PRZEWIDYWANE CENY ROPY NA RYNKU");
+	const string subtitle = string_create_from_cstring("PRZYCI$NIJ DOWOLNY KLAWISZ");
+	const label label = label_create(game->renderer, 38, title, white);
+	const struct label sublabel = label_create(game->renderer, 28, subtitle, white);
+	
+	const int x = 35;
+	const int y = 20;
+	const int width = SCREEN_WIDTH - x * 2;
+	const int height = SCREEN_HEIGHT - y * 2;
+
 	int total_time = 0;
 	double time_start = SDL_GetTicks();
 	double time = time_start;
@@ -55,19 +67,35 @@ void prices_loop(game_t* game, vector* prices) {
 		double delta = (SDL_GetTicks() - time) / 1000.0;
 		if (delta < game->max_frequency) continue;
 		time = SDL_GetTicks();
-		if (time - time_start > 3000) {
-			break;
-		}
+
 		mouse_update(game->mouse);
 
 		SDL_Event event;
-		while (SDL_PollEvent(&event)) {}
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				SDL_Quit();
+				exit(0);
+			}
+			if (event.type == SDL_KEYDOWN) {
+				label_free(&label);
+				label_free(&sublabel);
+				return;
+			}
+		}
 
-		SDL_SetRenderDrawColor(game->renderer, 91, 47, 115, 255);
+		SDL_SetRenderDrawColor(game->renderer, 143, 47, 101, 255);
 		SDL_RenderClear(game->renderer);
-
+		SDL_Rect subbg_rect = { x, y, width, height };
+		SDL_SetRenderDrawColor(game->renderer, 91, 47, 115, 255);
+		SDL_RenderFillRect(game->renderer, &subbg_rect);
+		
 		// draw graph
 		prices_draw(game->renderer, prices);
+		SDL_Rect yellow_rect = { x, y, width, label.rect.h - 15 };
+		SDL_SetRenderDrawColor(game->renderer, 204, 191, 69, 255);
+		SDL_RenderFillRect(game->renderer, &yellow_rect);
+		label_draw(&label, SCREEN_WIDTH / 2 - label.rect.w / 2, 10);
+		label_draw(&sublabel, SCREEN_WIDTH / 2 - sublabel.rect.w / 2, label.rect.h);
 
 		mouse_draw(game->renderer, game->mouse);
 		SDL_RenderPresent(game->renderer);
